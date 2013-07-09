6b38100e541fb8394997ddb536998213794e6d05
Glenn Adams <glenn@skynav.com>
2013-07-09 14:39:28 -0600

    [ttml11] Manually update from TTML10.

--- ttml11/spec/scm/load.scm
+++
@@ -1,7 +0,0 @@
-;;; -*-Scheme-*-
-
-(load-option 'xml)
-(with-working-directory-pathname (directory-pathname (current-load-pathname))
-  (lambda ()
-    (package/system-loader "dfxp" '() 'query)))
-(add-subsystem-identification! "DFXP" '(0 8))
